6c206d297771d278a21249ccd1ce713bdd8b1a1a
OWO Devs <developers.owo@gmail.com>
2024-04-23 12:35:52 +0200

    refactor: extract speed intensity calculation to its own file

---
+++ Application/SpeedIntensity.cs
@@ -0,0 +1,26 @@
+using OWOPluginSimHub.Domain;
+
+namespace OWOPluginSimHub.Application
+{
+    public class SpeedIntensity
+    {
+        public int From(WorldContext context)
+        {
+            if (context.KmHour <= 5) return 0;
+            if (context.Brake > 0) return (int)(context.KmHour / 3f);
+            switch (context.Gear)
+            {
+                case "0":
+                    return context.KmHour;
+                case "1":
+                    return 15 + context.KmHour;
+                case "2":
+                    return (int)(20 + context.KmHour / 1.3f);
+                case "3":
+                    return (int)(30 + context.KmHour / 1.6f);
+                default:
+                    return (int)(context.KmHour / 3f);
+            }
+        }
+    }
+}
