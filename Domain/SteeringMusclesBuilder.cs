82aa9d6f1e0c5145590e9b4fc335b8855100523d
OWO Devs <developers.owo@gmail.com>
2024-04-23 13:08:11 +0200

    feat: adjust values on steering

--- Domain/SteeringMusclesBuilder.cs
+++ Domain/SteeringMusclesBuilder.cs
@@ -11,11 +11,11 @@ namespace OWOPluginSimHub.Domain
     public class SteeringMusclesBuilder
     {
         public float AccelerationX { get; set; }
-        float SteerIntensity => Math.Clamp((System.Math.Abs(AccelerationX) - 3) / 10f, 0.1f, 0.3f);
+        float SteerIntensity => Math.Clamp((System.Math.Abs(AccelerationX) - 4) / 100f, 0.001f, 0.4f);
         static Muscle[] RightMuscles => new[] { Pectoral_R, Abdominal_R, Arm_R, Lumbar_R, Dorsal_R };
         static Muscle[] LeftMuscles => new[] { Pectoral_L, Abdominal_L, Arm_L, Lumbar_L, Dorsal_L };
-        bool TurningLeft => AccelerationX < -3;
-        Muscle[] SteeringMuscles => TurningLeft ? LeftMuscles : AccelerationX > 3 ? RightMuscles : Empty<Muscle>();
+        bool TurningLeft => AccelerationX < -2;
+        Muscle[] SteeringMuscles => TurningLeft ? LeftMuscles : AccelerationX > 2 ? RightMuscles : Empty<Muscle>();
         Muscle[] OppositeToSteering => SteeringMuscles.SequenceEqual(RightMuscles) ? LeftMuscles : RightMuscles;
         bool GoingStraight => SteeringMuscles.Length == 0;
 
@@ -29,7 +29,7 @@ namespace OWOPluginSimHub.Domain
 
             return AppendArms(allMuscles)
                 .ApplyIntensityMultiplier(1 + SteerIntensity, SteeringMuscles)
-                .ApplyIntensityMultiplier(1 - SteerIntensity, OppositeToSteering)
+                .ApplyIntensityMultiplier(Math.Clamp(1 - SteerIntensity, .1f, .3f), OppositeToSteering)
                 .ToArray();
         }
 
